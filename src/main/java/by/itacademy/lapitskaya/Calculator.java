26628c0622930ddcb9c4b26d211ef13a9e010462
HannaLapitskaya <anna.lapitskaya6@gmail.com>
2025-08-03 17:44:15 +0300

    add class Calculator()

--- src/main/java/by/itacademy/lapitskaya/Calculator.java
+++ src/main/java/by/itacademy/lapitskaya/Calculator.java
@@ -1,4 +1,51 @@
 package by.itacademy.lapitskaya;
 
-public class Calculator {
+public class Calculator implements ICalculator {
+    @Override
+    public int sum(int a, int b) {
+        long result = (long) a + b;
+
+        if (result < Integer.MIN_VALUE) {
+            throw new ArithmeticException(String.format("The sum of %d and %d is greater than min integer value - %d", a, b, Integer.MIN_VALUE));
+        } else if (result > Integer.MAX_VALUE) {
+            throw new ArithmeticException(String.format("The sum of %d and %d is greater than max integer value - %d", a, b, Integer.MAX_VALUE));
+        }
+
+        return a + b;
+    }
+
+    @Override
+    public int multiply(int a, int b) {
+        long result = (long) a * b;
+
+        if (result < Integer.MIN_VALUE) {
+            throw new ArithmeticException(String.format("The product of %d and %d is greater than min integer value - %d", a, b, Integer.MIN_VALUE));
+        } else if (result > Integer.MAX_VALUE) {
+            throw new ArithmeticException(String.format("The product of %d and %d is greater than max integer value - %d", a, b, Integer.MAX_VALUE));
+        }
+
+        return a * b;
+    }
+
+    @Override
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by 0 is not allowed");
+        }
+
+        return (double) a / b;
+    }
+
+    @Override
+    public int subtract(int a, int b) {
+        long result = (long) a - b;
+
+        if (result < Integer.MIN_VALUE) {
+            throw new ArithmeticException(String.format("The subtraction of %d and %d is greater than min integer value - %d", a, b, Integer.MIN_VALUE));
+        } else if (result > Integer.MAX_VALUE) {
+            throw new ArithmeticException(String.format("The sum of %d and %d is greater than max integer value - %d", a, b, Integer.MAX_VALUE));
+        }
+
+        return a - b;
+    }
 }
