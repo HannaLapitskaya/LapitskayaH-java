e7f96242ca1374d440329cbfc935887d00997a8f
HannaLapitskaya <anna.lapitskaya6@gmail.com>
2025-08-03 17:50:44 +0300

    add CalculatorTest() with sum and multiplication tests

--- src/test/java/by/itacademy/lapitskaya/CalculatorTest.java
+++ src/test/java/by/itacademy/lapitskaya/CalculatorTest.java
@@ -1,4 +1,190 @@
 package by.itacademy.lapitskaya;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
 public class CalculatorTest {
+    private ICalculator calculator = new Calculator();
+
+    @Test
+    @DisplayName("The sum of two positive numbers: 4 + 5 = 9")
+    @Order(1)
+    void testSumOfTwoPositiveNumbers() {
+        int expected = 9;
+        int actual = calculator.sum(4, 5);
+        Assertions.assertEquals(expected, actual, String.format("The expected sum of the numbers is %d, but actual sum is %d", expected, actual));
+    }
+
+    @Test
+    @DisplayName("The sum of two negative numbers: -15 + (-5) = -20")
+    @Order(2)
+    void testSumOfTwoNegativeNumbers() {
+        int expected = -20;
+        int actual = calculator.sum(-15, -5);
+        Assertions.assertEquals(expected, actual, String.format("The expected sum of the numbers is %d, but actual sum is %d", expected, actual));
+    }
+
+    @Test
+    @DisplayName("The sum of positive and negative numbers: 4 + 5 = 9")
+    @Order(3)
+    void testSumOfNegativeAndPositiveNumbers() {
+        int expected = -11;
+        int actual = calculator.sum(-30, 19);
+        Assertions.assertEquals(expected, actual, String.format("The expected sum of the numbers is %d, but actual sum is %d", expected, actual));
+    }
+
+    @Test
+    @DisplayName("The sum of two zeros: 0 + 0 = 0")
+    @Order(4)
+    void testSumOfTwoZeroes() {
+        int expected = 0;
+        int actual = calculator.sum(0, 0);
+        Assertions.assertEquals(expected, actual, String.format("The expected sum of the numbers is %d, but actual sum is %d", expected, actual));
+    }
+
+    @Test
+    @DisplayName("The sum of the number with zero: 90 + 0 = 0")
+    @Order(5)
+    void testSumOfNumberWithZero() {
+        int expected = 90;
+        int actual = calculator.sum(90, 0);
+        Assertions.assertEquals(expected, actual, String.format("The expected sum of the numbers is %d, but actual sum is %d", expected, actual));
+    }
+
+    @Test
+    @DisplayName("The sum of the numbers equal to the minimal value of int: (-1147483648) + (-1000000000) = -2147483648")
+    @Order(6)
+    void testSumOfNumbersEqualToTheMinValueOfInt() {
+        int expected = -2147483648;
+        int actual = calculator.sum(-1147483648, -1000000000);
+        Assertions.assertEquals(expected, actual, String.format("The expected sum of the numbers is %d, but actual sum is %d", expected, actual));
+    }
+
+    @Test
+    @DisplayName("The sum of the numbers equal to the minimal value of int: 2147483547 + 100 = 2147483647")
+    @Order(7)
+    void testSumOfNumbersEqualToTheMaxValueOfInt() {
+        int expected = 2147483647;
+        int actual = calculator.sum(2147483547, 100);
+        Assertions.assertEquals(expected, actual, String.format("The expected sum of the numbers is %d, but actual sum is %d", expected, actual));
+    }
+
+    @Test
+    @DisplayName("EXCEPTION: The sum of the numbers goes beyond the maximum limit of int")
+    @Order(8)
+    void testMaxSumException() {
+        Assertions.assertThrows(ArithmeticException.class, () -> {
+            calculator.sum(2147483646, 2);
+        });
+    }
+
+    @Test
+    @DisplayName("EXCEPTION: The sum of the numbers goes beyond the minimum limit of int")
+    @Order(9)
+    void testMinSumException() {
+        Assertions.assertThrows(ArithmeticException.class, () -> {
+            calculator.sum(-2147483648, -2);
+        });
+    }
+
+    @Test
+    @DisplayName("The multiplication of two positive numbers: 3 * 10 = 30")
+    @Order(10)
+    void testMultiplicationOfTwoPositiveNumbers() {
+        int expected = 30;
+        int actual = calculator.multiply(3, 10);
+        Assertions.assertEquals(expected, actual, String.format("The expected result of the multiplication is %d, but actual sum is %d", expected, actual));
+    }
+
+    @Test
+    @DisplayName("The multiplication of two negative numbers: (-5) * (-5) = 25")
+    @Order(11)
+    void testMultiplicationOfTwoNegativeNumbers() {
+        int expected = 25;
+        int actual = calculator.multiply(-5, -5);
+        Assertions.assertEquals(expected, actual, String.format("The expected result of the multiplication is %d, but actual sum is %d", expected, actual));
+    }
+
+    @Test
+    @DisplayName("The multiplication of two zeros: 0 + 0 = 0")
+    @Order(12)
+    void testMultiplicationOfTwoZeroes() {
+        int expected = 0;
+        int actual = calculator.multiply(0, 0);
+        Assertions.assertEquals(expected, actual, String.format("The expected result of the multiplication is %d, but actual sum is %d", expected, actual));
+    }
+
+    @Test
+    @DisplayName("The multiplication of the number with zero: 21 * 0 = 0")
+    @Order(13)
+    void testMultiplicationOfNumberWithZero() {
+        int expected = 0;
+        int actual = calculator.multiply(21, 0);
+        Assertions.assertEquals(expected, actual, String.format("The expected result of the multiplication is %d, but actual sum is %d", expected, actual));
+    }
+
+    @Test
+    @DisplayName("The multiplication of the numbers equal to the minimal value of int: 1073741824 * (-2) = -2147483648")
+    @Order(14)
+    void testMultiplicationOfNumbersEqualToTheMinValueOfInt() {
+        int expected = -2147483648;
+        int actual = calculator.multiply(1073741824, -2);
+        Assertions.assertEquals(expected, actual, String.format("The expected result of the multiplication is %d, but actual sum is %d", expected, actual));
+    }
+
+    @Test
+    @DisplayName("The multiplication of the numbers equal to the maximum value of int: 2147483547 + 100 = 2147483647")
+    @Order(15)
+    void testMultiplicationOfNumbersEqualToTheMaxValueOfInt() {
+        int expected = 2147483647;
+        int actual = calculator.multiply(2147483647, 1);
+        Assertions.assertEquals(expected, actual, String.format("The expected result of the multiplication is %d, but actual sum is %d", expected, actual));
+    }
+
+    @Test
+    @DisplayName("EXCEPTION: The multiplication of the numbers goes beyond the maximum limit of int")
+    @Order(16)
+    void testMaxMultiplicationException() {
+        Assertions.assertThrows(ArithmeticException.class, () -> {
+            calculator.multiply(214748364, 20);
+        });
+    }
+
+    @Test
+    @DisplayName("EXCEPTION: The multiplication of the numbers goes beyond the minimum limit of int")
+    @Order(17)
+    void testMinMultiplicationException() {
+        Assertions.assertThrows(ArithmeticException.class, () -> {
+            calculator.multiply(-2147483648, -2);
+        });
+    }
+
+    @Test
+    @DisplayName("EXCEPTION: multiplication beyond int boundaries")
+    @Order(18)
+    void testMultiplyException() {
+        Assertions.assertThrows(ArithmeticException.class, () -> {
+            calculator.multiply(100000, 10000000);
+        });
+    }
+
+    @Test
+    @DisplayName("This test of wrong sum should be failed")
+    @Order(19)
+    void testWrongSumOfTwoPositiveNumbers() {
+        int expected = 9;
+        int actual = calculator.sum(1, 5);
+        Assertions.assertEquals(expected, actual, String.format("The expected sum of the numbers is %d, but actual sum is %d", expected, actual));
+    }
+
+    @Test
+    @DisplayName("This test of wrong multiplication should be failed")
+    @Order(20)
+    void testWrongMultiplication() {
+        int expected = -90;
+        int actual = calculator.multiply(4, -5);
+        Assertions.assertEquals(expected, actual, String.format("The expected result of the multiplication is %d, but actual sum is %d", expected, actual));
+    }
 }
