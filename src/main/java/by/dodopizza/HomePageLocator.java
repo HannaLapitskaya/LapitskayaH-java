22caad1a8c7bb175f68809798ba4a1656306a2a1
HannaLapitskaya <anna.lapitskaya6@gmail.com>
2025-08-10 13:14:35 +0300

    add locators for login pop-up and main page

---
+++ src/main/java/by/dodopizza/HomePageLocator.java
@@ -0,0 +1,6 @@
+package by.dodopizza;
+
+public class HomePageLocator {
+    public static final String BUTTON_LOGIN_MAIN_PAGE = "//button[@data-testid = 'header_login']";
+    public static final String BUTTON_NAVIGATION_CART_MAIN_PAGE = "//button[@data-testid = 'navigation__cart']";
+}
---
+++ src/main/java/by/dodopizza/LoginPageLocator.java
@@ -0,0 +1,9 @@
+package by.dodopizza;
+
+public class LoginPageLocator {
+    public static final String HEADER_LOGIN_POPUP = "//div[@class = 'popup-inner sc-17puf9f-0 dyohjW']//h3";
+    public static final String TITLE_INPUT_FIELD_LOGIN_POPUP = "//span[@class = 'label  left']";
+    public static final String INPUT_FIELD_LOGIN_POPUP = "//input[@data-testid = 'checkout-form__phone-input']";
+    public static final String INPUT_CHECKOUT_FORM_LOGIN_POPUP = "//input[@data-testid = 'checkout-form__phone-input']";
+    public static final String BUTTON_CLOSE_LOGIN_POPUP = "//button[@class = 'popup-close-button']";
+}
