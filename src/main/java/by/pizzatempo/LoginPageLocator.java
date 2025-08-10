bfef1652371f1a57bca7d20165e15abd4e08b3ef
HannaLapitskaya <anna.lapitskaya6@gmail.com>
2025-08-10 13:35:58 +0300

    add locators for login page

---
+++ src/main/java/by/pizzatempo/LoginPageLocator.java
@@ -0,0 +1,10 @@
+package by.pizzatempo;
+
+public class LoginPageLocator {
+    public static final String HEADER_LOGIN = "//form[@class = 'auth']/div";
+    public static final String INPUT_EMAIL = "//input[@name = 'astroauth_login']";
+    public static final String INPUT_PASSWORD = "//input[@name = 'astroauth_pass']";
+    public static final String INPUT_SUBMIT = "//input[@name = 'astroauth_submit']";
+    public static final String BUTTON_REGISTRATION = "//a[@href = 'https://www.pizzatempo.by/registration/']";
+    public static final String BUTTON_FORGOT_PASSWORD = "//a[@href = 'https://www.pizzatempo.by/registration/forgot/']";
+}
